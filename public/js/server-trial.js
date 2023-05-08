a0ad7209c8e909ce2983def500f70cfd2239605b
imposterjump <125587220+imposterjump@users.noreply.github.com>
2023-05-08 19:35:27 +0200

    created files and trying things

---
+++ public/js/server-trial.js
@@ -0,0 +1,11 @@
+import * as http from 'node:http';
+
+function open_ServiceWorkerRegistration() {
+
+}
+
+
+http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World!');
+}).listen(8080);
